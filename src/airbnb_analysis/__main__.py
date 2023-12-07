812ad7de166aae02ed4936f310e651b46fd61ecc
MAPALO LUKASHI <mlukashi@mail.yu.edu>
2023-12-06 23:15:59 -0500

    Update main.py

--- src/airbnb_analysis/__main__.py
+++ src/airbnb_analysis/__main__.py
@@ -1,6 +1,6 @@
-# __main__.py within the airbnb_analysis package
+# __main__.py for the airbnb_analysis package
 
-import matplotlib.pyplot as plt
+import pandas as pd
 from airbnb_analysis.summary import DataSummary
 from airbnb_analysis.analysis import ExploratoryDataAnalysis
 from airbnb_analysis.inference import Inference
@@ -8,44 +8,40 @@ from airbnb_analysis.modeling import Modeling
 
 def main():
     """
-    Run Airbnb data analysis as a script, processing data through various stages of analysis.
+    Run Airbnb data analysis as a complete script, encompassing summary statistics,
+    exploratory data analysis, inference statistics, and predictive modeling.
     """
-    print("------------------------------------------------")
-    print("Airbnb Data Analysis")
-    print("------------------------------------------------")
-    
-    # Load data
-    data_path = "data/listings.csv" 
+    # Assuming the data is pre-loaded and cleaned in a CSV file
+    data_path = 'data/airbnb_listings.csv'  # Replace with the correct path to your data file
     data = pd.read_csv(data_path)
-    
-    # Data Summary
-    data_summary = DataSummary(data)
-    data_summary.data_info()
-    data_summary.missing_value_summary()
 
+    # Data summarization
+    summary = DataSummary(data)
+    summary.display_head()
+    summary.display_tail()
+    summary.data_info()
+    
     # Exploratory Data Analysis
     eda = ExploratoryDataAnalysis(data)
     eda.plot_price_distribution()
-    eda.plot_price_distribution_seaborn()
     eda.plot_minimum_nights_distribution()
-    eda.plot_minimum_nights_distribution_seaborn()
-
-    # Inference
+    eda.plot_number_of_reviews_distribution()
+    eda.plot_pairplot(data, hue='room_type')  # Replace 'room_type' with actual column name if different
+    eda.plot_correlation_heatmap(data)
+    
+    # Inference Statistics
     inference = Inference(data)
-    price_room_type_test = inference.hypothesis_test_price_room_type()
-    print(f"ANOVA test for price across room types: {price_room_type_test}")
-
+    result = inference.hypothesis_test_price_room_type()
+    print("ANOVA test result for price across room types:", result)
+    
     # Predictive Modeling
-    target = 'price'  # Update with actual target variable
+    target = 'price'  # Replace with the actual target column name if different
     modeling = Modeling(data, target)
     modeling.train()
     modeling_results = modeling.evaluate()
-    print(f"Modeling results: {modeling_results}")
-    
-    # Visualizations
+    print("Model evaluation results:", modeling_results)
     modeling.plot_residuals()
     modeling.plot_actual_vs_predicted()
     
-    # Show any plots that were created
-    plt.show()
-
+if __name__ == "__main__":
+    main()
