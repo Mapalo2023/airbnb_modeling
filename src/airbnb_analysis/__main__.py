be8b98784905365d2af9bb7d6b94fc7f375ed1a5
MAPALO LUKASHI <129587512+Mapalo2023@users.noreply.github.com>
2023-12-06 23:19:00 -0500

    Update __main__.py

--- src/airbnb_analysis/__main__.py
+++ src/airbnb_analysis/__main__.py
@@ -1,4 +1,4 @@
-# __main__.py for the airbnb_analysis package
+# __main__.py for the airbnb_analysis package, Enable running `python -m project_package` 
 
 import pandas as pd
 from airbnb_analysis.summary import DataSummary
@@ -11,8 +11,7 @@ def main():
     Run Airbnb data analysis as a complete script, encompassing summary statistics,
     exploratory data analysis, inference statistics, and predictive modeling.
     """
-    # Assuming the data is pre-loaded and cleaned in a CSV file
-    data_path = 'data/airbnb_listings.csv'  # Replace with the correct path to your data file
+    data_path = 'data/listings.csv'  
     data = pd.read_csv(data_path)
 
     # Data summarization
@@ -26,7 +25,7 @@ def main():
     eda.plot_price_distribution()
     eda.plot_minimum_nights_distribution()
     eda.plot_number_of_reviews_distribution()
-    eda.plot_pairplot(data, hue='room_type')  # Replace 'room_type' with actual column name if different
+    eda.plot_pairplot(data, hue='room_type') 
     eda.plot_correlation_heatmap(data)
     
     # Inference Statistics
@@ -35,7 +34,7 @@ def main():
     print("ANOVA test result for price across room types:", result)
     
     # Predictive Modeling
-    target = 'price'  # Replace with the actual target column name if different
+    target = 'price'  
     modeling = Modeling(data, target)
     modeling.train()
     modeling_results = modeling.evaluate()
